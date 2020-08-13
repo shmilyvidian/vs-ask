1bde7748d60fc32b254a3330658d19d83dae22d2
frosv <616342684@qq.com>
2020-08-13 16:29:59 +0800

    fix: 调整删除弹窗样式

--- src/components/modalWrap/modalWrap.tsx
+++ src/components/modalWrap/modalWrap.tsx
@@ -2,73 +2,71 @@ import React from "react";
 import { Modal, Button } from "antd";
 import classnames from "classnames";
 
+import './index.less'
+
 export interface IFooter {
-	okClsName: string;
-	okTxt: string;
-	cancleClsName: string;
-	cancleTxt: string;
-	onOk: Function;
-	onCancel: Function;
+  okClsName: string;
+  okTxt: string;
+  cancleClsName: string;
+  cancleTxt: string;
+  onOk: Function;
+  onCancel: Function;
 }
 
 interface IModal {
-	modal_visible: boolean;
-	footer: IFooter;
-	content: any;
-	type: string;
+  modal_visible: boolean;
+  footer: IFooter;
+  content: any;
+  type: string;
 }
 
-let _style = {
-	btn: {
-		marginRight: 30,
-	},
-	contain: {
-		padding: "10px 20px",
-		color: "black",
-	},
-};
-const ModalWrap = ({ modal_visible, footer, content, type }: IModal) => {
-	return (
-		<Modal
-			visible={modal_visible}
-			closable={false}
-			wrapClassName="ModalWrap__style"
-			onCancel={() => {
-				footer.onCancel();
-			}}
-			title="删除"
-			footer={
-				footer ? (
-					<div style={_style.btn}>
-						<Button
-							size="small"
-							onClick={() => {
-								footer.onCancel();
-							}}
-							className={classnames(footer.cancleClsName, {
-								hide: !footer.cancleTxt,
-							})}
-						>
-							{footer.cancleTxt}
-						</Button>
-						<Button
-							size="small"
-							onClick={() => {
-								footer.onOk();
-							}}
-							className={classnames(footer.okClsName, {
-								hide: !footer.okTxt || type === "info",
-							})}
-						>
-							{footer.okTxt}
-						</Button>
-					</div>
-				) : null
-			}
-		>
-			<div style={_style.contain}>{content}</div>
-		</Modal>
-	);
+const ModalWrap = ({
+  modal_visible,
+  footer,
+  content,
+  type
+}: IModal) => {
+  return (
+    <Modal
+      visible={true}
+      closable={false}
+      wrapClassName="ModalWrap__style"
+      onCancel={() => {
+        footer.onCancel();
+      }}
+      title="删除"
+      footer={
+        footer ? (
+          <div className="footer-wrapper">
+            <Button
+              size="small"
+              onClick={() => {
+                footer.onCancel();
+              }}
+              className={classnames(footer.cancleClsName, {
+                hide: !footer.cancleTxt,
+              },'footer-btn-common','footer-cancel')}
+            >
+              {footer.cancleTxt}
+            </Button>
+            <Button
+              size="small"
+              onClick={() => {
+                footer.onOk();
+              }}
+              className={classnames(footer.okClsName, {
+                hide: !footer.okTxt || type === "info",
+              },'footer-btn-common')}
+            >
+              {footer.okTxt}
+            </Button>
+          </div>
+        ) : null
+      }
+    >
+      {content}
+    </Modal>
+  );
 };
 
 export default ModalWrap;
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -5,91 +5,94 @@ import TableWrap, { DelFnType } from "components/tableWrap";
 import ModalWrap, { IFooter } from "components/modalWrap/modalWrap";
 import { InfoCircleFilled } from "@ant-design/icons";
 
-const content = (
-	<div>
-		<div>
-			<InfoCircleFilled style={{ color: "#faa64b" }} /> 确定要删除吗？
+import './index.less'
+
+// 删除模板内容
+const deleteDialogContent = (
+  <div className="delete-wrapper">
+    <div className="delete-top">
+      <InfoCircleFilled className="delete-icon" /> 确定要删除吗？
 		</div>
-		<div>问题被删除之后不能撤销</div>
-	</div>
+    <div className="delete-second">问题被删除之后不能撤销！</div>
+  </div>
 );
 
 const Home = () => {
-	const infos: IBtn[] = [
-		{
-			name: "新建问题",
-			tip: "",
-			type: "primary",
-			icon: "add",
-			callback: () => history.push("/add"),
-		},
-		{
-			name: "删除",
-			tip: "删除",
-			type: "default",
-			icon: "delete",
-			isDelete: false,
-			disabled: true,
-			callback: () => {},
-		},
-	];
-	const history = useHistory();
-	const [btnInfos, setBtnInfos] = useState<IBtn[]>(infos);
+  const infos: IBtn[] = [
+    {
+      name: "新建问题",
+      tip: "",
+      type: "primary",
+      icon: "add",
+      callback: () => history.push("/add"),
+    },
+    {
+      name: "删除",
+      tip: "删除",
+      type: "default",
+      icon: "delete",
+      isDelete: false,
+      disabled: true,
+      callback: () => { },
+    },
+  ];
+  const history = useHistory();
+  const [btnInfos, setBtnInfos] = useState<IBtn[]>(infos);
 
-	const [confirmVisible, setConfirmVisible] = useState<boolean>(false);
+  const [confirmVisible, setConfirmVisible] = useState<boolean>(false);
 
-	const [okCancelFn, setOkCancelFn] = useState<{ fn: Function }>({
-		fn: () => {},
-	});
+  const [okCancelFn, setOkCancelFn] = useState<{ fn: Function }>({
+    fn: () => { },
+  });
 
-	const modalControl = (v: boolean) => {
-		setConfirmVisible(v);
-	};
-	const confirmfooter: IFooter = {
-		okClsName: "ant-btn-primary",
-		okTxt: "确定",
-		cancleClsName: "",
-		cancleTxt: "取消",
-		onOk: () => {
-			okCancelFn.fn();
-		},
-		onCancel: () => {
-			modalControl(false);
-		},
-	};
-	function onCallback(isDelete: DelFnType) {
-		if (isDelete.isDel) {
-			let origin = infos;
-			let delbtn = origin[1];
-			delbtn.disabled = false;
-			delbtn.isDelete = true;
-			delbtn.callback = () => modalControl(true);
-			setBtnInfos([...origin]);
-		} else {
-			let origin = infos;
-			let delbtn = origin[1];
-			delbtn.disabled = true;
-			delbtn.isDelete = false;
-			setBtnInfos([...origin]);
-		}
-		const fn = () => {
-			isDelete.delfn();
-			modalControl(false);
-		};
-		setOkCancelFn({ fn });
-	}
-	return (
-		<div className="vs-ask__home">
-			<ButtonWrap btnInfos={btnInfos} />
-			<TableWrap callback={onCallback} />
-			<ModalWrap
-				modal_visible={confirmVisible}
-				footer={confirmfooter}
-				content={content}
-				type="info"
-			/>
-		</div>
-	);
+  const modalControl = (v: boolean) => {
+    setConfirmVisible(v);
+  };
+  const confirmfooter: IFooter = {
+    okClsName: "ant-btn-primary",
+    okTxt: "确定",
+    cancleClsName: "",
+    cancleTxt: "取消",
+    onOk: () => {
+      okCancelFn.fn();
+    },
+    onCancel: () => {
+      modalControl(false);
+    },
+  };
+  function onCallback(isDelete: DelFnType) {
+    if (isDelete.isDel) {
+      let origin = infos;
+      let delbtn = origin[1];
+      delbtn.disabled = false;
+      delbtn.isDelete = true;
+      delbtn.callback = () => modalControl(true);
+      setBtnInfos([...origin]);
+    } else {
+      let origin = infos;
+      let delbtn = origin[1];
+      delbtn.disabled = true;
+      delbtn.isDelete = false;
+      setBtnInfos([...origin]);
+    }
+    const fn = () => {
+      isDelete.delfn();
+      modalControl(false);
+    };
+    setOkCancelFn({ fn });
+  }
+  return (
+    <div className="vs-ask__home">
+      <ButtonWrap btnInfos={btnInfos} />
+      <TableWrap callback={onCallback} />
+      <ModalWrap
+        modal_visible={confirmVisible}
+        footer={confirmfooter}
+        content={deleteDialogContent}
+        type="info"
+      />
+    </div>
+  );
 };
 
 export default Home;
