d023e260d8c8f9ce188bed93b158905cf73485c3
panwl2018 <pan7359127@@>
2020-08-14 18:30:51 +0800

    feat:编辑页面样式调整

--- src/views/add/index.tsx
+++ src/views/add/index.tsx
@@ -185,32 +185,37 @@ const Add = () => {
       onFinish={onFinish}
       validateMessages={validateMessages}
     >
+      <Form.Item>
+        <div className={defaultTitle ? 'title-wrapper' : 'hidden'}>
+          编辑内容
+        </div>
+      </Form.Item>
       <Form.Item
         name={["user", "name"]}
         label="标题"
         rules={[
           { required: true },
           {
-            max:20,
-            message:"标题内容不能超过20个字符"
+            max: 20,
+            message: "标题内容不能超过20个字符"
           }
         ]}
         initialValue={defaultTitle}
       >
-        <Input className="input-textarea" placeholder="请输入标题"/>
+        <Input className="input-textarea" placeholder="请输入标题" />
       </Form.Item>
       <Form.Item
         name={["user", "introduction"]}
         label="问题描述"
         rules={[{ required: true },
-          {
-            max:200,
-            message:"问题描述不能超过200个字符"
-          }
+        {
+          max: 200,
+          message: "问题描述不能超过200个字符"
+        }
         ]}
         initialValue={defaultIntroduction}
       >
-        <Input.TextArea rows={7} className="input-textarea" placeholder="请输入问题描述"/>
+        <Input.TextArea rows={7} className="input-textarea" placeholder="请输入问题描述" />
       </Form.Item>
       <Form.Item className="img-upload" name={["user", "image"]} label="图片">
         <Upload
@@ -270,11 +275,11 @@ const Add = () => {
                   >
                     <img src={download} className="operate-enclosure operate-enclosure-download" alt="" />
                     <img className="operate-enclosure operate-enclosure-delete" src={deleteImg}
-                     alt=""
-                     onClick={() => {
-                      customFilist.splice(i, 1);
-                      setCustomFilist([...customFilist]);
-                    }} />
+                      alt=""
+                      onClick={() => {
+                        customFilist.splice(i, 1);
+                        setCustomFilist([...customFilist]);
+                      }} />
                   </div>
                 </div>
               );
@@ -291,7 +296,7 @@ const Add = () => {
           }
         ]}
       >
-        <Input className="input-textarea" placeholder="请输入回访电话"/>
+        <Input className="input-textarea" placeholder="请输入回访电话" />
       </Form.Item>
       <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 8 }}>
         <Button type="primary" htmlType="submit" className="m__r--10">
