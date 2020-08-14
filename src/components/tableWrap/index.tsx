58372473d32d9abe0e6fa7fef737edd8b577e8e3
frosv <616342684@qq.com>
2020-08-14 11:24:54 +0800

    feat: 调整图片样式 调整删除后无法同步数据问题

--- src/components/tableWrap/index.tsx
+++ src/components/tableWrap/index.tsx
@@ -25,7 +25,7 @@ export interface DelFnType {
   delfn: () => void;
 }
 
-export const data: DataType[] = [
+export let data: DataType[] = [
   {
     key: 0,
     name: "文字消息发出后无法撤回",
@@ -36,82 +36,6 @@ export const data: DataType[] = [
   },
   { key: 1, name: "认证不成功", startTime: "2020-08-06", state: 1, introduction: "测试", telphone: "13965971356" },
 ];
-
-// const filterIcon = <CaretDownOutlined />;
-// const columns: ColumnsType<DataType> = [
-// 	{
-// 		title: "标题",
-// 		dataIndex: "name",
-// 	},
-// 	{
-// 		title: "开始时间",
-// 		dataIndex: "startTime",
-// 		width: 200,
-// 	},
-// 	{
-// 		title: "状态",
-// 		dataIndex: "state",
-// 		width: 200,
-// 		filterIcon: filterIcon,
-// 		filters: [
-// 			{
-// 				text: "已完成",
-// 				value: 0,
-// 			},
-// 			{
-// 				text: "处理中",
-// 				value: 1,
-// 			},
-// 		],
-// 		onFilter: (value: string | number | boolean, record: DataType) => {
-// 			return record.state === value;
-// 		},
-// 		render: (_: any, record: DataType) => {
-// 			return <span>{record.state === 0 ? "已完成" : "处理中"}</span>;
-// 		},
-// 	},
-// ];
-
-// const TableWrap = ({ callback }: ITable) => {
-// 	const [selectedRowKeys, setRowKeys] = useState([]);
-// 	const [innerData, setInnerData] = useState<DataType[]>(data);
-
-// 	const rowSelection: TableRowSelection<DataType> = {
-// 		selectedRowKeys,
-// 		onChange: (selectedRowKeys: any, selectedRows: DataType[]) => {
-// 			const delfn = () => {
-// 				let remain = innerData;
-// 				selectedRows.forEach((v) => {
-// 					remain = remain.filter((l) => l.key !== v.key);
-// 				});
-// 				setInnerData(remain);
-// 			};
-// 			let isDel: boolean;
-// 			if (selectedRowKeys.length > 0) {
-// 				isDel = true;
-// 				callback({
-// 					delfn,
-// 					isDel,
-// 				});
-// 			} else {
-// 				isDel = false;
-// 				callback({
-// 					delfn,
-// 					isDel,
-// 				});
-// 			}
-// 			setRowKeys(selectedRowKeys);
-// 		},
-// 	};
-
-// 	return (
-// 		<Table
-// 			rowSelection={rowSelection}
-// 			columns={columns}
-// 			dataSource={innerData}
-// 		/>
-// 	);
-// };
 
 const TableWrap = ({ callback }: ITable) => {
   const [selectedRowKeys, setRowKeys] = useState([]);
@@ -223,6 +147,9 @@ const TableWrap = ({ callback }: ITable) => {
           remain = remain.filter((l) => l.key !== v.key);
         });
         setInnerData(remain);
+
+        // 保持数据同步
+        data = remain
         if (filterData.length > 0) {
           setFilterData([]);
           setFilterState([false, false]);
