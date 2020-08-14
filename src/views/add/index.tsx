198c27834cc9886f40a0d6f2f6daff9357f829d0
panwl2018 <pan7359127@@>
2020-08-14 15:33:23 +0800

    feat:新增页面附件预览

--- src/views/add/index.tsx
+++ src/views/add/index.tsx
@@ -88,7 +88,7 @@ const Add = () => {
     }
     return e && e.fileList;
   };
-  const [customFilist, setCustomFilist] = useState<UploadFile<any>[]>([]);
+  const [customFilist, setCustomFilist] = useState<any[]>([]);
 
   const handleChange = (e: any) => {
     const file = e.file;
@@ -121,6 +121,30 @@ const Add = () => {
           status: 'done',
           url: jpg2
         }
+      ])
+
+      // 显示默认附件
+      setCustomFilist([
+        {
+          uid: '1',
+          name: '附件名称1.pdf',
+          status: 'done',
+          response: 'Server Error 500',
+          url: 'http://www.baidu.com/xxx.png'
+        },
+        {
+          uid: '2',
+          name: '附件名称2.pdf',
+          status: 'error',
+          url: 'http://www.baidu.com/yyy.png',
+        },
+        {
+          uid: '3',
+          name: '附件名称3.pdf',
+          status: 'done',
+          response: 'Server Error 500', // custom error message to show
+          url: 'http://www.baidu.com/zzz.png',
+        },
       ])
     }
     return v;
