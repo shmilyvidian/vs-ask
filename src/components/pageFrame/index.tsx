859546acb182ae265b27aaa637bbe6e997d7b838
panwl2018 <pan7359127@@>
2020-08-14 09:42:21 +0800

    feat:合并代码

--- src/components/pageFrame/index.tsx
+++ src/components/pageFrame/index.tsx
@@ -1,30 +1,36 @@
 import React from "react"
 import { Route } from 'react-router-dom'
+import { useHistory } from "react-router";
 import Home from 'views/home'
 import Add from 'views/add'
 import './index.less'
 
 const PageFrame = () => {
+  const history = useHistory();
 
-    return (
-        <>
-            <header className="pageFrame-header">
-                <div>
-                    <img src={require('assets/svg/logo.svg')} className="pageFrame-header_logo" alt="logo" />
-                </div>
-                <div className="pageFrame-header_user">
-                    <img src={require('assets/svg/logo.svg')} className="pageFrame-header_user_avatar" alt="avatar" />
-                    <span>lisa</span>
-                </div>
-            </header>
-            <div className="pageFrame-container">
-                <div className="pageFrame-container_content">
-                    <Route path="/home" component={Home}></Route>
-                    <Route path="/add" component={Add}></Route>
-                </div>
-            </div>
-        </>
-    )
+  // 回到首页
+  function linkIndex(){
+    history.push("/");
+  }
+  return (
+    <>
+      <header className="pageFrame-header">
+        <div onClick={linkIndex}>
+          <img src={require('assets/svg/logo.svg')} className="pageFrame-header_logo" alt="logo" />
+        </div>
+        <div className="pageFrame-header_user">
+          <img src={require('assets/svg/logo.svg')} className="pageFrame-header_user_avatar" alt="avatar" />
+          <span>lisa</span>
+        </div>
+      </header>
+      <div className="pageFrame-container">
+        <div className="pageFrame-container_content">
+          <Route path="/home" component={Home}></Route>
+          <Route path="/add" component={Add}></Route>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default PageFrame
