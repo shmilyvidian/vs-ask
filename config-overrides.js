f265e5596bcc6fb9f56b1de9dcc23e42ff094975
panwl2018 <pan7359127@@>
2020-08-17 20:10:15 +0800

    feat:打包体积优化

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,38 @@
 const { override, fixBabelImports, addLessLoader, addWebpackAlias, addTslintLoader } = require('customize-cra')
 const path = require('path')
 
+// 关闭mapsource
+const rewiredMap = () => config => {
+    config.devtool = config.mode === 'development' ? 'cheap-module-source-map' : false;
+    return config;
+};
+
+const optimize =()=>config=>{
+  config.optimization ={
+    splitChunks:{
+      chunks: 'initial',
+      minSize: 30000,
+      maxSize: 1000000,
+      minChunks: 1,
+      maxInitialRequests: 6,
+      cacheGroups:{
+        vendors: {  //自定义打包模块
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10, //优先级，先打包到哪个组里面，值越大，优先级越高
+          filename: 'static/js/vendors.js',
+        },
+        default: { //默认打包模块
+          priority: -20,
+          reuseExistingChunk: true, //模块嵌套引入时，判断是否复用已经被打包的模块
+          filename: 'static/js/common.js'
+        }
+      }
+    }
+  }
+  return config;
+}
+
+
 module.exports = override(
   addTslintLoader(),
   fixBabelImports('import', {
@@ -15,5 +47,7 @@ module.exports = override(
     views: path.resolve(__dirname, './src/views'),
     styles: path.resolve(__dirname, './src/styles')
   }),
-  addLessLoader()
+  addLessLoader(),
+  rewiredMap(),
+  optimize()
 )
